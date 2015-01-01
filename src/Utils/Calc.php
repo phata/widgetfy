4e4a6330fbca9b6b14242e1a74f0ff1224d0e198
Koala Yeung <koalay@gmail.com>
2015-01-01 17:01:02 +0800

    Added Utils/Calc for misc calculation

---
+++ src/Utils/Calc.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * class Phata\Widgetfy\Utils\Calc
+ *
+ * Licence:
+ *
+ * This file is part of Widgetfy.
+ *
+ * Widgetfy is free software: you can redistribute
+ * it and/or modify it under the terms of the GNU
+ * Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Widgetfy is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser
+ * General Public Licensefor more details.
+ *
+ * You should have received a copy of the GNU Lesser
+ * General Public License along with Widgetfy.  If
+ * not, see <http://www.gnu.org/licenses/lgpl.html>.
+ *
+ * Description:
+ *
+ * This file defines Phata\Widgetfy\Utils\Calc
+ * which is an utility class to help the Widgetfy
+ * library to function.
+ *
+ * @package   Widgetfy
+ * @author    Koala Yeung <koalay@gmail.com>
+ * @copyright 2014 Koala Yeung
+ * @licence   http://www.gnu.org/licenses/lgpl.html
+ * @link      http://github.com/Phata/Widgetfy
+ */
+
+namespace Phata\Widgetfy\Utils;
+
+class Calc{
+
+    /**
+     * calculates the height of a rectangle with a given
+     * height and aspect factor
+     *
+     * @param int $width width of the rectangle
+     * @param float $factor the result of height / width
+     *        default to be ratio of a 16:9 rectangle
+     * @return int round up result of height
+     */
+    public static function retHeight($width, $aspect_factor=0.5625) {
+        return ceil($width * $aspect_factor);
+    }
+
+}
---
+++ tests/Utils/CalcTest.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * Unit test for Phata\Widgetfy\Utils\Calc
+ *
+ * Licence:
+ *
+ * This file is part of Widgetfy.
+ *
+ * Widgetfy is free software: you can redistribute
+ * it and/or modify it under the terms of the GNU
+ * Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Widgetfy is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser
+ * General Public Licensefor more details.
+ *
+ * You should have received a copy of the GNU Lesser
+ * General Public License along with Widgetfy.  If
+ * not, see <http://www.gnu.org/licenses/lgpl.html>.
+ *
+ * Description:
+ *
+ * This file is a unit test for
+ * - Phata\Widgetfy\Utils\Calc
+ *
+ * @package   Widgetfy
+ * @author    Koala Yeung <koalay@gmail.com>
+ * @copyright 2014 Koala Yeung
+ * @licence   http://www.gnu.org/licenses/lgpl.html
+ * @link      http://github.com/Phata/Widgetfy
+ */
+
+use Phata\Widgetfy\Utils\Calc as Calc;
+
+class CalcTest extends PHPUnit_Framework_TestCase {
+
+    public function testCalc_default() {
+        $width = 640;
+        $this->assertEquals(360, Calc::retHeight($width));
+    }
+
+    public function testCalc_16_9() {
+        $width = 640;
+        $this->assertEquals(360, Calc::retHeight($width, 0.5625));
+    }
+
+    public function testCalc_4_3() {
+        $width = 640;
+        $this->assertEquals(480, Calc::retHeight($width, 0.75));
+    }
+
+}
