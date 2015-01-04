4e226c7f8dd64625f436e5a95ca6a72492aaac81
Koala Yeung <koalay@gmail.com>
2015-01-04 17:05:59 +0800

    Add test to the core interface
    Added test for Phata\Widgetfy itself.

---
+++ tests/Core/WidgetfyTest.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * Unit test for Phata\Widgetfy
+ *
+ * Licence:
+ *
+ * This file is part of Widgetfy.
+ *
+ * Widgetfy is free software: you can redistribute
+ * it and/or modify it under the terms of the GNU
+ * Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Widgetfy is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser
+ * General Public Licensefor more details.
+ *
+ * You should have received a copy of the GNU Lesser
+ * General Public License along with Widgetfy.  If
+ * not, see <http://www.gnu.org/licenses/lgpl.html>.
+ *
+ * Description:
+ *
+ * This file is a unit test for
+ * - Phata\Widgetfy
+ *
+ * @package   Widgetfy
+ * @author    Koala Yeung <koalay@gmail.com>
+ * @copyright 2014 Koala Yeung
+ * @licence   http://www.gnu.org/licenses/lgpl.html
+ * @link      http://github.com/Phata/Widgetfy
+ */
+
+use Phata\Widgetfy as Widgetfy;
+
+class WidgetfyTest extends PHPUnit_Framework_TestCase {
+
+    public function testGeneric() {
+        $url = 'https://youtube.com/watch?v=PBLuP2JZcEg';
+        $options = array('width'=>640);
+
+        // test returning embed code
+        $embed = Widgetfy::translate($url);
+        $this->assertEquals($embed['html'],
+            '<iframe width="640" height="360" '.
+            'src="//www.youtube.com/embed/PBLuP2JZcEg" frameborder="0" allowfullscreen></iframe>'
+        );
+        $this->assertEquals($embed['type'], 'iframe');
+        $this->assertEquals($embed['dimension']->factor, 0.5625);
+    }
+
+}
